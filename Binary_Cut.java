a8acf4505eb0d4afef33a26391ea30ca799a6e10
yash7xm <yash.7xm@gmail.com>
2024-05-10 21:14:42 +0530

    D

---
+++ Binary_Cut.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Binary_Cut {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        
+        while(t-- > 0) {
+            String s = in.next();
+            int count = 0;
+            boolean flag = false;
+
+            for(int i=0; i<s.length();){
+                while(i<s.length() && s.charAt(i) == '1') {
+                    i++;
+                    flag = true;
+                }
+                if(flag) {
+                    count++;
+                    flag = false;
+                }
+
+                while(i<s.length() && s.charAt(i) == '0') {
+                    i++;
+                    flag = true;
+                }
+
+                if(flag) {
+                    while(i<s.length() && s.charAt(i) == '1') {
+                        i++;
+                    }
+                    count++;
+                    flag = false;
+                }
+            }
+
+            System.out.println(count);
+        }
+        in.close();
+    }
+}
