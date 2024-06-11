5e55aa75817eb9e569d83d0057b227e54a3718fc
yash7xm <yash.7xm@gmail.com>
2024-06-11 19:57:12 +0530

    magnitude

--- Magnitude.java
+++ Magnitude.java
@@ -4,6 +4,7 @@ public class Magnitude {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        int mod = 998244353;
         while (t-- > 0) {
             int n = in.nextInt();
             int[] arr = new int[n];
@@ -14,18 +15,25 @@ public class Magnitude {
 
             long c1 = 0;
             long c2 = 0;
+            int abs = 0;
+            int nrm = 0;
             if (arr[0] >= 0) {
                 c1 = arr[0];
                 c2 = arr[0];
+                abs = 1;
+                nrm = 1;
             } else {
                 c1 = arr[0];
                 c2 = -arr[0];
+                abs = 1;
+                nrm = 0;
             }
             long[] a = new long[4];
             for (int i = 1; i < n; i++) {
                 a[0] = Math.abs(arr[i] + c1);
-                a[1] = Math.abs(arr[i] + c2);
-                a[2] = arr[i] + c1;
+                a[1] = arr[i] + c1;
+
+                a[2] = Math.abs(arr[i] + c2);
                 a[3] = arr[i] + c2;
 
                 long max = Long.MIN_VALUE;
@@ -35,13 +43,21 @@ public class Magnitude {
                     min = Math.min(a[j], min);
                 }
 
-                System.out.println("c1 -> " + c1 + " " + "c2 -> " + c2);
-
                 c1 = max;
                 c2 = min;
+
+                if (a[0] == c1 || a[2] == c1) {
+                    abs++;
+                }
+                if (a[1] == c1 || a[3] == c1) {
+                    nrm++;
+                }
+
+                System.out.println("abs-> " + abs + "nrm-> " + nrm);
+
             }
 
-            System.out.println(Math.max(c1, c2));
+            System.out.println((abs * nrm) % mod);
         }
         in.close();
     }
