6ef8aa502ad4aa43b68385a6b0b45b85a7af34c8
yash7xm <yash.7xm@gmail.com>
2024-07-05 00:28:54 +0530

    permutation_swap

--- tle-900/Permutation_Swap.java
+++ tle-900/Permutation_Swap.java
@@ -11,16 +11,22 @@ public class Permutation_Swap {
                 arr[i] = in.nextInt();
             }
 
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < n; i++) {
-                int diff = Math.abs(arr[i] - (i + 1));
-                if (diff < min && diff != 0) {
-                    min = diff;
-                }
+            int a = Math.abs(arr[0] - 1);
+            for (int i = 2; i <= n; i++) {
+                int b = Math.abs(arr[i - 1] - i);
+                a = gcd(a, b);
             }
 
-            System.out.println(min);
+            System.out.println(a);
         }
         in.close();
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
     }
 }
