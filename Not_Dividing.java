7cae254dbb691d34cb8cde17341005c764e76903
yash7xm <yash.7xm@gmail.com>
2024-07-05 23:24:19 +0530

    Not_Dividing

--- Not_Dividing.java
+++ Not_Dividing.java
@@ -6,24 +6,21 @@ public class Not_Dividing {
         int t = in.nextInt();
         while (t-- > 0) {
             int n = in.nextInt();
-            long[] arr = new long[n];
+            int[] arr = new int[n];
 
             for (int i = 0; i < n; i++) {
-                arr[i] = in.nextLong();
+                arr[i] = in.nextInt();
+                if (arr[i] == 1)
+                    arr[i]++;
             }
 
-            for (int i = 0; i < n - 1; i++) {
-                if (arr[i] % arr[i + 1] == 0) {
-                    long temp = arr[i];
-                    if ((temp + 1) % arr[i + 1] != 0) {
-                        arr[i] = temp + 1;
-                    } else if ((temp + 2) % arr[i + 1] != 0) {
-                        arr[i] = temp + 2;
-                    }
+            for (int i = 1; i < n; i++) {
+                if (arr[i] % arr[i - 1] == 0) {
+                    arr[i]++;
                 }
             }
 
-            for (long el : arr) {
+            for (int el : arr) {
                 System.out.print(el + " ");
             }
             System.out.println();
