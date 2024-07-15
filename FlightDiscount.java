eb6b45d9cf563ebfd877a093ed1c659dc88e73d5
yash7xm <yash.7xm@gmail.com>
2024-07-15 14:08:57 +0530

    FlightDiscount

---
+++ FlightDiscount.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class FlightDiscount {
+
+    public static class Pair implements Comparable<Pair> {
+        int vtx;
+        long wt;
+        boolean c;
+
+        Pair(int vtx, long wt, boolean c) {
+            this.vtx = vtx;
+            this.wt = wt;
+            this.c = c;
+        }
+
+        public int compareTo(Pair o) {
+            return Long.compare(this.wt, o.wt);
+        }
+    }
+
+    static ArrayList<ArrayList<Pair>> graph;
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = scn.nextInt();
+            int v = scn.nextInt();
+            int wt = scn.nextInt();
+
+            graph.get(u).add(new Pair(v, wt, false));
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(1, 0, false));
+        long[][] path = new long[n + 1][2];
+        for (long[] p : path) {
+            Arrays.fill(p, Long.MAX_VALUE);
+        }
+
+        path[1][0] = 0;
+
+        while (pq.size() > 0) {
+            Pair rem = pq.poll();
+
+            if (rem.wt > path[rem.vtx][rem.c ? 1 : 0]) {
+                continue;
+            }
+            for (Pair nbr : graph.get(rem.vtx)) {
+                if (!rem.c) {
+                    long newWt = rem.wt + nbr.wt;
+                    if (path[nbr.vtx][0] > newWt) {
+                        path[nbr.vtx][0] = newWt;
+                        pq.add(new Pair(nbr.vtx, newWt, false));
+                    }
+
+                    long disWt = rem.wt + nbr.wt / 2;
+                    if (path[nbr.vtx][1] > disWt) {
+                        path[nbr.vtx][1] = disWt;
+                        pq.add(new Pair(nbr.vtx, disWt, true));
+                    }
+                } else {
+                    long newWt = rem.wt + nbr.wt;
+                    if (path[nbr.vtx][1] > newWt) {
+                        path[nbr.vtx][1] = newWt;
+                        pq.add(new Pair(nbr.vtx, newWt, true));
+                    }
+                }
+            }
+        }
+
+        long res = Math.min(path[n][0], path[n][1]);
+        System.out.println(res);
+
+        scn.close();
+    }
+}
--- a.cpp
+++ a.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 #include <climits>
+
 using namespace std;
 
-int main() {
-    int n, m, q;
-    cin >> n >> m >> q;
+class Pair
+{
+public:
+    int vtx;
+    long long wt;
+    bool c;
 
-    vector<vector<long long>> dist(n, vector<long long>(n, LLONG_MAX));
-    
-    for (int i = 0; i < n; i++) {
-        dist[i][i] = 0;  // Distance from a node to itself is zero
+    Pair(int vtx, long long wt, bool c) : vtx(vtx), wt(wt), c(c) {}
+
+    bool operator>(const Pair &other) const
+    {
+        return this->wt > other.wt;
     }
+};
 
-    for (int i = 0; i < m; i++) {
+vector<vector<Pair>> graph;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    graph.resize(n + 1);
+
+    for (int i = 0; i < m; i++)
+    {
         int u, v;
         long long wt;
         cin >> u >> v >> wt;
-        u--; v--;  // Adjusting for 0-based indexing
-        dist[u][v] = min(dist[u][v], wt);
-        dist[v][u] = min(dist[v][u], wt);  // If the graph is bidirectional
+        graph[u].emplace_back(v, wt, false);
     }
 
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] != LLONG_MAX && dist[k][j] != LLONG_MAX) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
+    pq.emplace(1, 0, false);
+
+    vector<vector<long long>> path(n + 1, vector<long long>(2, LLONG_MAX));
+    path[1][0] = 0;
+
+    while (!pq.empty())
+    {
+        Pair rem = pq.top();
+        pq.pop();
+
+        if (rem.wt > path[rem.vtx][rem.c ? 1 : 0])
+        {
+            continue;
+        }
+
+        for (const Pair &nbr : graph[rem.vtx])
+        {
+            if (!rem.c)
+            {
+                long long newWt = rem.wt + nbr.wt;
+                if (path[nbr.vtx][0] > newWt)
+                {
+                    path[nbr.vtx][0] = newWt;
+                    pq.emplace(nbr.vtx, newWt, false);
+                }
+
+                long long disWt = rem.wt + nbr.wt / 2;
+                if (path[nbr.vtx][1] > disWt)
+                {
+                    path[nbr.vtx][1] = disWt;
+                    pq.emplace(nbr.vtx, disWt, true);
+                }
+            }
+            else
+            {
+                long long newWt = rem.wt + nbr.wt;
+                if (path[nbr.vtx][1] > newWt)
+                {
+                    path[nbr.vtx][1] = newWt;
+                    pq.emplace(nbr.vtx, newWt, true);
                 }
             }
         }
     }
 
-    for (int i = 0; i < q; i++) {
-        int x, y;
-        cin >> x >> y;
-        x--; y--;  // Adjusting for 0-based indexing
-        cout << (dist[x][y] == LLONG_MAX ? -1 : dist[x][y]) << endl;
-    }
+    long long res = min(path[n][0], path[n][1]);
+    cout << res << endl;
 
     return 0;
 }
