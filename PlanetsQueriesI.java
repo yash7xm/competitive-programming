ca009355298878f1c14c16e63b89275ccec5d5d1
yash7xm <yash.7xm@gmail.com>
2024-07-18 00:49:55 +0530

    Planet Queries

--- PlanetsQueriesI.java
+++ PlanetsQueriesI.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class PlanetsQueriesI {
 
@@ -9,21 +10,94 @@ public class PlanetsQueriesI {
         int n = p.length;
         table = new int[MAX][n];
 
-        for(int i=0; i<n; i++) {
-            table[0][p[i]] = i;
+        for (int i = 0; i < n; i++) {
+            table[0][i] = p[i];
+        }
+
+        for (int i = 1; i < MAX; i++) {
+            for (int j = 0; j < n; j++) {
+                table[i][j] = table[i - 1][table[i - 1][j]];
+            }
         }
     }
 
+    static int query(int a, int k) {
+        for (int i = 0; i < MAX; i++) {
+            int mask = (1 << i);
+            if ((mask & k) > 0) {
+                a = table[i][a];
+            }
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        FastReader scn = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
         int n = scn.nextInt();
         int q = scn.nextInt();
 
         int[] p = new int[n];
         for (int i = 0; i < n; i++) {
-            p[i] = scn.nextInt()-1;
+            p[i] = scn.nextInt() - 1;
         }
 
-        scn.close();
+        build(p);
+
+        for (int i = 0; i < q; i++) {
+            int a = scn.nextInt() - 1;
+            int k = scn.nextInt();
+
+            out.println(query(a, k) + 1);
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
--- a.cpp
+++ a.cpp
@@ -1,74 +1,64 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Edge {
-    int vtx;
-    long long wt;
-    Edge(int v, long long w) : vtx(v), wt(w) {}
-    bool operator>(const Edge& other) const {
-        return wt > other.wt;
-    }
-};
+const int MAX = 30;
+vector<vector<int>> table;
 
-struct Node {
-    long long min;
-    int ways;
-    int minc;
-    int maxc;
-    Node(long long m, int w, int minVtx, int maxVtx) : min(m), ways(w), minc(minVtx), maxc(maxVtx) {}
-};
+void build(vector<int> &p)
+{
+    int n = p.size();
+    table.assign(MAX, vector<int>(n));
 
-vector<vector<Edge>> graph;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    graph.resize(n + 1);
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        long long wt;
-        cin >> u >> v >> wt;
-        graph[u].emplace_back(v, wt);
+    for (int i = 0; i < n; i++)
+    {
+        table[0][i] = p[i];
     }
 
-    vector<Node> dp(n + 1, Node(LLONG_MAX, 0, INT_MAX, INT_MIN));
-    dp[1] = Node(0, 1, 0, 0);
-    int mod = 1000000007;
-
-    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
-    pq.emplace(1, 0);
-
-    while (!pq.empty()) {
-        Edge rem = pq.top();
-        pq.pop();
-
-        long long newDist = rem.wt;
-        if (newDist > dp[rem.vtx].min)
-            continue;
-
-        for (Edge& nbr : graph[rem.vtx]) {
-            newDist = rem.wt + nbr.wt;
-            if (newDist < dp[nbr.vtx].min) {
-                dp[nbr.vtx].min = newDist;
-                dp[nbr.vtx].ways = dp[rem.vtx].ways;
-                dp[nbr.vtx].minc = dp[rem.vtx].minc + 1;
-                dp[nbr.vtx].maxc = dp[rem.vtx].maxc + 1;
-                pq.emplace(nbr.vtx, newDist);
-            } else if (newDist == dp[nbr.vtx].min) {
-                dp[nbr.vtx].ways = (dp[nbr.vtx].ways + dp[rem.vtx].ways) % mod;
-                dp[nbr.vtx].minc = min(dp[nbr.vtx].minc, dp[rem.vtx].minc + 1);
-                dp[nbr.vtx].maxc = max(dp[nbr.vtx].maxc, dp[rem.vtx].maxc + 1);
-            }
+    for (int i = 1; i < MAX; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            table[i][j] = table[i - 1][table[i - 1][j]];
         }
     }
+}
 
-    cout << dp[n].min << " " << dp[n].ways << " " << dp[n].minc << " " << dp[n].maxc << endl;
+int query(int a, int k)
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        if ((k & (1 << i)) != 0)
+        {
+            a = table[i][a];
+        }
+    }
+    return a;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> p(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p[i];
+        p[i]--;
+    }
+
+    build(p);
+
+    for (int i = 0; i < q; i++)
+    {
+        int a, k;
+        cin >> a >> k;
+        a--;
+        cout << query(a, k) + 1 << '\n';
+    }
 
     return 0;
 }
