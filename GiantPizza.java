10c4c97bd849bf604142f6292610ebd59644729a
yash7xm <yash.7xm@gmail.com>
2024-07-22 13:10:19 +0530

    Giant Pizza

--- GiantPizza.java
+++ GiantPizza.java
@@ -1,5 +1,166 @@
+import java.util.*;
+import java.io.*;
+
 public class GiantPizza {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    static List<List<Integer>> adj;
+    static List<List<Integer>> adjRev;
+    static int component[];
+    static boolean assignment[], vis[];
+    static Stack<Integer> st;
+    static int n, m;
+
+    static int negate(int x) {
+        return x < n ? x + n : x - n;
+    }
+
+    static void addClause(int x, int y) {
+        adj.get(negate(x)).add(y);
+        adj.get(negate(y)).add(x);
+        adjRev.get(y).add(negate(x));
+        adjRev.get(x).add(negate(y));
+    }
+
+    static void dfs1(int u) {
+        vis[u] = true;
+        for (int v : adj.get(u)) {
+            if (!vis[v]) {
+                dfs1(v);
+            }
+        }
+        st.push(u);
+    }
+
+    static void dfs2(int u, int comp) {
+        component[u] = comp;
+        for (int v : adjRev.get(u)) {
+            if (component[v] == -1) {
+                dfs2(v, comp);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+        m = in.nextInt();
+        n = in.nextInt();
+
+        adj = new ArrayList<>();
+        adjRev = new ArrayList<>();
+        for (int i = 0; i < 2 * n; i++) {
+            adj.add(new ArrayList<>());
+            adjRev.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            String line = in.nextLine();
+            String[] parts = line.split(" ");
+            int x, y;
+
+            if (parts[0].equals("+")) {
+                x = Integer.parseInt(parts[1]) - 1;
+            } else {
+                x = negate(Integer.parseInt(parts[1]) - 1);
+            }
+
+            if (parts[2].equals("+")) {
+                y = Integer.parseInt(parts[3]) - 1;
+            } else {
+                y = negate(Integer.parseInt(parts[3]) - 1);
+            }
+
+            addClause(x, y);
+        }
+
+        st = new Stack<>();
+        vis = new boolean[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            if (!vis[i]) {
+                dfs1(i);
+            }
+        }
+
+        component = new int[2 * n];
+        Arrays.fill(component, -1);
+        int comp = 0;
+        while (!st.isEmpty()) {
+            int u = st.pop();
+            if (component[u] == -1) {
+                dfs2(u, comp++);
+            }
+        }
+
+        boolean flag = true;
+        assignment = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (component[i] == component[negate(i)]) {
+                flag = false;
+                break;
+            }
+
+            assignment[i] = component[i] > component[negate(i)];
+        }
+
+        if (!flag) {
+            out.println("IMPOSSIBLE");
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (assignment[i]) {
+                    out.print("+ ");
+                } else {
+                    out.print("- ");
+                }
+            }
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
--- a.cpp
+++ a.cpp
@@ -1,74 +1,110 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_HEIGHT = 20;
-vector<vector<int>> binaryLifting;
-vector<int> arr, len;
-vector<bool> visited;
+vector<vector<int>> adj, adjRev;
+vector<int> component;
+vector<bool> assignment, vis;
+stack<int> st;
+int n, m;
 
-void dfs(int node) {
-    if (visited[node]) {
-        return;
-    }
-    visited[node] = true;
-    dfs(arr[node]);
-    binaryLifting[node][0] = arr[node];
-    len[node] = len[binaryLifting[node][0]] + 1;
-    for (int level = 1; level < MAX_HEIGHT; level++) {
-        binaryLifting[node][level] = binaryLifting[binaryLifting[node][level - 1]][level - 1];
-    }
+int negateVar(int x) {
+    return x < n ? x + n : x - n;
 }
 
-int jump(int a, int dist) {
-    if (dist < 0) return -1;
-    int level = 0;
-    while (dist != 0) {
-        if ((dist & 1) == 1) {
-            a = binaryLifting[a][level];
+void addClause(int x, int y) {
+    adj[negateVar(x)].push_back(y);
+    adj[negateVar(y)].push_back(x);
+    adjRev[y].push_back(negateVar(x));
+    adjRev[x].push_back(negateVar(y));
+}
+
+void dfs1(int u) {
+    vis[u] = true;
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            dfs1(v);
         }
-        level++;
-        dist = dist / 2;
     }
-    return a;
+    st.push(u);
+}
+
+void dfs2(int u, int comp) {
+    component[u] = comp;
+    for (int v : adjRev[u]) {
+        if (component[v] == -1) {
+            dfs2(v, comp);
+        }
+    }
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, q;
-    cin >> n >> q;
-    arr.resize(n + 1);
-    binaryLifting.assign(n + 1, vector<int>(MAX_HEIGHT));
-    len.assign(n + 1, 0);
-    visited.assign(n + 1, false);
+    cin >> m >> n;
 
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
-    }
+    adj.resize(2 * n);
+    adjRev.resize(2 * n);
+    for (int i = 0; i < m; i++) {
+        string sign1, sign2;
+        int x, y;
+        cin >> sign1 >> x >> sign2 >> y;
 
-    for (int i = 1; i <= n; i++) {
-        if (!visited[i]) {
-            dfs(i);
-        }
-    }
-
-    stringstream output;
-    for (int i = 0; i < q; i++) {
-        int a, b;
-        cin >> a >> b;
-        int aa = jump(a, len[a]);
-        int answer;
-        if (jump(a, len[a] - len[b]) == b) {
-            answer = len[a] - len[b];
-        } else if (jump(aa, len[aa] - len[b]) == b) {
-            answer = (len[aa] - len[b]) + len[a];
+        if (sign1 == "+") {
+            x = x - 1;
         } else {
-            answer = -1;
+            x = negateVar(x - 1);
         }
-        output << answer << "\n";
+
+        if (sign2 == "+") {
+            y = y - 1;
+        } else {
+            y = negateVar(y - 1);
+        }
+
+        addClause(x, y);
     }
 
-    cout << output.str();
+    vis.resize(2 * n, false);
+    for (int i = 0; i < 2 * n; i++) {
+        if (!vis[i]) {
+            dfs1(i);
+        }
+    }
+
+    component.resize(2 * n, -1);
+    int comp = 0;
+    while (!st.empty()) {
+        int u = st.top();
+        st.pop();
+        if (component[u] == -1) {
+            dfs2(u, comp++);
+        }
+    }
+
+    bool flag = true;
+    assignment.resize(n);
+    for (int i = 0; i < n; i++) {
+        if (component[i] == component[negateVar(i)]) {
+            flag = false;
+            break;
+        }
+
+        assignment[i] = component[i] > component[negateVar(i)];
+    }
+
+    if (!flag) {
+        cout << "IMPOSSIBLE" << endl;
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (assignment[i]) {
+                cout << "+ ";
+            } else {
+                cout << "- ";
+            }
+        }
+        cout << endl;
+    }
+
     return 0;
 }
