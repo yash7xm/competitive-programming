42ac2a64ea00a144fbddf2ad23e2c3688e27d908
yash7xm <yash.7xm@gmail.com>
2024-07-22 17:30:01 +0530

    Mail Delivery

---
+++ MailDelivery.java
@@ -0,0 +1,124 @@
+import java.util.*;
+import java.io.*;
+
+public class MailDelivery {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static class Pair {
+        int node;
+        int idx;
+
+        Pair(int node, int idx) {
+            this.node = node;
+            this.idx = idx;
+        }
+    }
+
+    static List<List<Pair>> graph;
+    static int deg[];
+    static boolean seen[];
+    static List<Integer> path;
+
+    static void dfs(int node) {
+        while (!graph.get(node).isEmpty()) {
+            Pair edge = graph.get(node).remove(graph.get(node).size() - 1);
+            if (seen[edge.idx])
+                continue;
+            seen[edge.idx] = true;
+            dfs(edge.node);
+        }
+        path.add(node);
+    }
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        deg = new int[n + 1];
+        for (int i = 1; i <= m; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+
+            graph.get(u).add(new Pair(v, i));
+            graph.get(v).add(new Pair(u, i));
+            deg[u]++;
+            deg[v]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (deg[i] % 2 != 0) {
+                out.println("IMPOSSIBLE");
+                out.flush();
+                return;
+            }
+        }
+
+        seen = new boolean[m + 1];
+        path = new ArrayList<>();
+
+        dfs(1);
+
+        if (path.size() != m + 1) {
+            out.println("IMPOSSIBLE");
+        } else {
+            for (int i = path.size() - 1; i >= 0; i--) {
+                out.print(path.get(i) + " ");
+            }
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
--- a.cpp
+++ a.cpp
@@ -1,69 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> val, component, topSort;
-vector<vector<int>> adj, adjRev, adjScc;
-vector<long long> sccVal;
-vector<bool> vis;
-stack<int> st;
+struct Pair {
+    int node, idx;
+    Pair(int node, int idx) : node(node), idx(idx) {}
+};
 
-void dfs1(int u) {
-    vis[u] = true;
-    for (int v : adj[u]) {
-        if (!vis[v]) {
-            dfs1(v);
-        }
-    }
-    st.push(u);
-}
+vector<vector<Pair>> graph;
+vector<int> deg;
+vector<bool> seen;
+vector<int> path;
 
-void dfs2(int u, int comp, long long &sum) {
-    component[u] = comp;
-    sum += val[u];
-    for (int v : adjRev[u]) {
-        if (component[v] == -1) {
-            dfs2(v, comp, sum);
-        }
+void dfs(int node) {
+    while (!graph[node].empty()) {
+        Pair edge = graph[node].back();
+        graph[node].pop_back();
+        if (seen[edge.idx])
+            continue;
+        seen[edge.idx] = true;
+        dfs(edge.node);
     }
-}
-
-void build(int n, int comp) {
-    adjScc.assign(comp, vector<int>());
-    for (int u = 1; u <= n; u++) {
-        for (int v : adj[u]) {
-            if (component[u] != component[v]) {
-                adjScc[component[u]].push_back(component[v]);
-            }
-        }
-    }
-}
-
-void topologicalSort(int n) {
-    vector<int> indeg(n, 0);
-    for (int i = 0; i < n; i++) {
-        for (int v : adjScc[i]) {
-            indeg[v]++;
-        }
-    }
-
-    queue<int> q;
-    for (int i = 0; i < n; i++) {
-        if (indeg[i] == 0) {
-            q.push(i);
-        }
-    }
-
-    while (!q.empty()) {
-        int rem = q.front();
-        q.pop();
-        topSort.push_back(rem);
-        for (int v : adjScc[rem]) {
-            indeg[v]--;
-            if (indeg[v] == 0) {
-                q.push(v);
-            }
-        }
-    }
+    path.push_back(node);
 }
 
 int main() {
@@ -73,54 +30,38 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    val.resize(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> val[i];
-    }
+    graph.resize(n + 1);
+    deg.resize(n + 1, 0);
 
-    adj.assign(n + 1, vector<int>());
-    adjRev.assign(n + 1, vector<int>());
-    for (int i = 0; i < m; i++) {
+    for (int i = 1; i <= m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adjRev[v].push_back(u);
+        graph[u].emplace_back(v, i);
+        graph[v].emplace_back(u, i);
+        deg[u]++;
+        deg[v]++;
     }
 
-    vis.assign(n + 1, false);
-    for (int u = 1; u <= n; u++) {
-        if (!vis[u]) {
-            dfs1(u);
+    for (int i = 1; i <= n; i++) {
+        if (deg[i] % 2 != 0) {
+            cout << "IMPOSSIBLE" << endl;
+            return 0;
         }
     }
 
-    component.assign(n + 1, -1);
-    int comp = 0;
-    sccVal.clear();
-    while (!st.empty()) {
-        int u = st.top();
-        st.pop();
-        if (component[u] == -1) {
-            long long sum = 0;
-            dfs2(u, comp++, sum);
-            sccVal.push_back(sum);
+    seen.resize(m + 1, false);
+    path.clear();
+
+    dfs(1);
+
+    if (path.size() != m + 1) {
+        cout << "IMPOSSIBLE" << endl;
+    } else {
+        for (int i = path.size() - 1; i >= 0; i--) {
+            cout << path[i] << " ";
         }
+        cout << endl;
     }
-
-    topSort.clear();
-    build(n, comp);
-    topologicalSort(comp);
-
-    vector<long long> dp(comp, 0);
-    for (int u : topSort) {
-        dp[u] += sccVal[u];
-        for (int v : adjScc[u]) {
-            dp[v] = max(dp[v], dp[u]);
-        }
-    }
-
-    long long maxCoins = *max_element(dp.begin(), dp.end());
-    cout << maxCoins << '\n';
 
     return 0;
 }
