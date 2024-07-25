b1e417845520a79d1dae51b331cd5348f027478e
yash7xm <yash.7xm@gmail.com>
2024-07-25 19:49:42 +0530

    Prefix Sum Queries

---
+++ PrefixSumQueries.java
@@ -0,0 +1,136 @@
+import java.util.*;
+import java.io.*;
+
+public class PrefixSumQueries {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    static final int N = 200010;
+    static final int S = 1 << 18;
+
+    static int n, q;
+    static long[] a = new long[N];
+    static Node[] tt = new Node[S << 1];
+
+    static class Node {
+        long sum, pref;
+
+        Node(long sum, long pref) {
+            this.sum = sum;
+            this.pref = pref;
+        }
+
+        Node(long x) {
+            this.sum = x;
+            this.pref = Math.max(0, x);
+        }
+
+        static Node combine(Node a, Node b) {
+            return new Node(a.sum + b.sum, Math.max(a.pref, a.sum + b.pref));
+        }
+    }
+
+    static void build(int k, int l, int r) {
+        if (l == r) {
+            tt[k] = new Node(a[l]);
+            return;
+        }
+        int m = (l + r) >> 1;
+        build(k << 1, l, m);
+        build(k << 1 | 1, m + 1, r);
+        tt[k] = Node.combine(tt[k << 1], tt[k << 1 | 1]);
+    }
+
+    static void update(int i, long x, int k, int l, int r) {
+        if (l == r) {
+            tt[k] = new Node(x);
+            return;
+        }
+        int m = (l + r) >> 1;
+        if (i <= m)
+            update(i, x, k << 1, l, m);
+        else
+            update(i, x, k << 1 | 1, m + 1, r);
+        tt[k] = Node.combine(tt[k << 1], tt[k << 1 | 1]);
+    }
+
+    static Node query(int ql, int qr, int k, int l, int r) {
+        if (ql > r || qr < l)
+            return new Node(0);
+        if (ql <= l && qr >= r)
+            return tt[k];
+        int m = (l + r) >> 1;
+        Node q1 = query(ql, qr, k << 1, l, m);
+        Node q2 = query(ql, qr, k << 1 | 1, m + 1, r);
+        return Node.combine(q1, q2);
+    }
+
+    public static void main(String[] args) {
+        n = in.nextInt();
+        q = in.nextInt();
+        for (int i = 1; i <= n; ++i) {
+            a[i] = in.nextLong();
+        }
+        build(1, 1, n);
+
+        for (int i = 0; i < q; ++i) {
+            int t = in.nextInt();
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if (t == 1) {
+                update(x, a[x] = y, 1, 1, n);
+            } else {
+                out.println(query(x, y, 1, 1, n).pref);
+            }
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
--- a.cpp
+++ a.cpp
@@ -1,107 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Queries {
-    int type, a, b;
-    Queries(int type, int a, int b) : type(type), a(a), b(b) {}
-};
+const int N = 200010;
+const int S = 1 << 18;
 
-vector<int> arr, tree;
-vector<int> allVals;
-vector<Queries> quers;
-unordered_map<int, int> compressMap;
+int n, q, a[N];
 
-void compressIndexes() {
-    sort(allVals.begin(), allVals.end());
-    allVals.erase(unique(allVals.begin(), allVals.end()), allVals.end());
+struct node {
+	long long sum, pref;
+	node(long long sum, long long pref) : sum(sum), pref(pref) {}
+	node(long long x = 0) : sum(x), pref(max(0LL, x)) {}
+	friend node operator+(const node &a, const node &b) {
+		return {a.sum + b.sum, max(a.pref, a.sum + b.pref)};
+	}
+} tt[S << 1];
 
-    for (int i = 0; i < allVals.size(); i++) {
-        compressMap[allVals[i]] = i;
-    }
+void build(int k = 1, int l = 1, int r = n) {
+	if (l == r) {
+		tt[k] = node(a[l]);
+		return;
+	}
+	int m = (l + r) >> 1;
+	build(k << 1, l, m);
+	build(k << 1 | 1, m + 1, r);
+	tt[k] = tt[k << 1] + tt[k << 1 | 1];
 }
 
-void update(int node, int start, int end, int idx, int val) {
-    if (start == end) {
-        tree[node] += val;
-    } else {
-        int mid = (start + end) / 2;
-        if (start <= idx && idx <= mid) {
-            update(2 * node, start, mid, idx, val);
-        } else {
-            update(2 * node + 1, mid + 1, end, idx, val);
-        }
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
-    }
+void update(int i, int x, int k = 1, int l = 1, int r = n) {
+	if (l == r) {
+		tt[k] = node(x);
+		return;
+	}
+	int m = (l + r) >> 1;
+	if (i <= m) update(i, x, k << 1, l, m);
+	else update(i, x, k << 1 | 1, m + 1, r);
+	tt[k] = tt[k << 1] + tt[k << 1 | 1];
 }
 
-int query(int node, int start, int end, int l, int r) {
-    if (r < start || end < l) {
-        return 0;
-    }
-    if (l <= start && end <= r) {
-        return tree[node];
-    }
-    int mid = (start + end) / 2;
-    int left_query = query(2 * node, start, mid, l, r);
-    int right_query = query(2 * node + 1, mid + 1, end, l, r);
-    return left_query + right_query;
-}
-
-void build() {
-    for (int i = 0; i < arr.size(); i++) {
-        int k = compressMap[arr[i]];
-        update(1, 0, allVals.size() - 1, k, 1);
-    }
+node query(int ql, int qr, int k = 1, int l = 1, int r = n) {
+	if (ql > r || qr < l) return 0;
+	if (ql <= l && qr >= r) return tt[k];
+	int m = (l + r) >> 1;
+	node q1 = query(ql, qr, k << 1, l, m);
+	node q2 = query(ql, qr, k << 1 | 1, m + 1, r);
+	return q1 + q2;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, q;
-    cin >> n >> q;
-
-    arr.resize(n);
-    allVals.reserve(n + 2 * q);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        allVals.push_back(arr[i]);
-    }
-
-    quers.reserve(q);
-    for (int i = 0; i < q; i++) {
-        char type;
-        int a, b;
-        cin >> type >> a >> b;
-
-        if (type == '?') {
-            quers.emplace_back(1, a, b);
-            allVals.push_back(a);
-            allVals.push_back(b);
-        } else {
-            quers.emplace_back(2, a, b);
-            allVals.push_back(b);
-        }
-    }
-
-    compressIndexes();
-
-    tree.resize(4 * allVals.size(), 0);
-    build();
-
-    for (const auto& quer : quers) {
-        if (quer.type == 1) {
-            int a = compressMap[quer.a];
-            int b = compressMap[quer.b];
-            cout << query(1, 0, allVals.size() - 1, a, b) << "\n";
-        } else {
-            int k = quer.a - 1;
-            int x = quer.b;
-            update(1, 0, allVals.size() - 1, compressMap[arr[k]], -1);
-            arr[k] = x;
-            update(1, 0, allVals.size() - 1, compressMap[x], 1);
-        }
-    }
-
-    return 0;
+	scanf("%d%d", &n, &q);
+	for (int i = 1; i <= n; ++i) scanf("%d", a + i);
+	build();
+	for (int i = 0, t, x, y; i < q; ++i) {
+		scanf("%d%d%d", &t, &x, &y);
+		if (t == 1) update(x, a[x] = y);
+		else printf("%lld\n", query(x, y).pref);
+	}
 }
