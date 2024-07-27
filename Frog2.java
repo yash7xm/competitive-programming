415bf1d1eb07437f92238b603793ebf68e482e60
yash7xm <yash.7xm@gmail.com>
2024-07-27 20:03:41 +0530

    Frog2

---
+++ Frog2.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+
+public class Frog2 {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j <= k + i && j < n; j++) {
+                dp[i] = Math.min(dp[i], Math.abs(arr[i] - arr[j]) + dp[j]);
+            }
+        }
+
+        out.println(dp[0]);
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
