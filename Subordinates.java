724fe3ccb0a07713292742a204255cd9540ea755
yash7xm <yash.7xm@gmail.com>
2024-09-01 13:31:24 +0530

    solved https://www.cses.fi/problemset/task/1674/

---
+++ Subordinates.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Subordinates {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static List<List<Integer>> tree;
+    static int[] res;
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        tree = new ArrayList<>();
+        res = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        for (int i = 2; i <= n; i++) {
+            int u = in.nextInt();
+            tree.get(u).add(i);
+        }
+
+        dfs(1);
+
+        for (int i = 1; i <= n; i++) {
+            out.print(res[i] + " ");
+        }
+        out.flush();
+    }
+
+    private static int dfs(int u) {
+        int sum = 0;
+        for (int child : tree.get(u)) {
+            sum += dfs(child) + 1;
+        }
+        res[u] = sum;
+        return sum;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
--- a.cpp
+++ a.cpp
@@ -1,76 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    long long sum, maxsubsum, prefix, suffix;
-    Node(long long sum = 0, long long maxsubsum = 0, long long prefix = 0, long long suffix = 0) 
-        : sum(sum), maxsubsum(maxsubsum), prefix(prefix), suffix(suffix) {}
-};
+vector<vector<int>> tree;
+vector<int> res;
 
-vector<int> arr;
-vector<Node> tree;
-
-Node merge(Node left, Node right) {
-    Node node;
-    node.sum = left.sum + right.sum;
-    node.prefix = max(left.prefix, left.sum + right.prefix);
-    node.suffix = max(right.suffix, right.sum + left.suffix);
-    node.maxsubsum = max({left.maxsubsum, right.maxsubsum, left.suffix + right.prefix});
-    return node;
-}
-
-void build(int node, int start, int end) {
-    if (start == end) {
-        tree[node] = Node(arr[start], arr[start], arr[start], arr[start]);
-        return;
+int dfs(int u) {
+    int sum = 0;
+    for (int child : tree[u]) {
+        sum += dfs(child) + 1;
     }
-
-    int mid = (start + end) / 2;
-    build(2 * node, start, mid);
-    build(2 * node + 1, mid + 1, end);
-    tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
-}
-
-void update(int node, int start, int end, int i, int x) {
-    if (start == end) {
-        tree[node] = Node(x, x, x, x);
-        return;
-    }
-
-    int mid = (start + end) / 2;
-    if (i <= mid) {
-        update(2 * node, start, mid, i, x);
-    } else {
-        update(2 * node + 1, mid + 1, end, i, x);
-    }
-    tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
+    res[u] = sum;
+    return sum;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
 
-    int n, q;
-    cin >> n >> q;
+    int n;
+    cin >> n;
 
-    arr.resize(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+    tree.resize(n + 1);
+    res.resize(n + 1);
+
+    for (int i = 2; i <= n; i++) {
+        int u;
+        cin >> u;
+        tree[u].push_back(i);
     }
 
-    tree.resize(4 * n);
-    build(1, 0, n - 1);
+    dfs(1);
 
-    for (int i = 0; i < q; ++i) {
-        int k, x;
-        cin >> k >> x;
-        --k;
-        arr[k] = x;
-        update(1, 0, n - 1, k, x);
-        long long res = tree[1].maxsubsum;
-        if (res < 0) res = 0;
-        cout << res << '\n';
+    for (int i = 1; i <= n; i++) {
+        cout << res[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
