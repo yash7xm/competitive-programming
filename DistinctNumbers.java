2f990f0b1ede88207e1090567f1d01061c601c02
yash7xm <yash.7xm@gmail.com>
2024-09-17 19:00:15 +0530

    solved https://www.cses.fi/problemset/task/1621/

---
+++ DistinctNumbers.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class DistinctNumbers {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(in.nextInt());
+        }
+
+        out.println(set.size());
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
