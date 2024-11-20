a21d4e390fb7611ff4a188315438185bf3d08355
yash7xm <yash.7xm@gmail.com>
2024-11-20 17:23:20 +0530

    solved cses sum of two values

---
+++ Sorting_and_Searching/SumofTwoValues.java
@@ -0,0 +1,64 @@
+package Sorting_and_Searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class SumofTwoValues {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int x = in.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int complement = x - arr[i];
+            if (map.containsKey(complement)) {
+                out.println((map.get(complement) + 1) + " " + (i + 1));
+                out.flush();
+                return;
+            }
+            map.put(arr[i], i);
+        }
+
+        out.println("IMPOSSIBLE");
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
