2aaa0382fd83f66bfe62a6c8d07d0f8793d27170
yash7xm <yash.7xm@gmail.com>
2024-11-20 18:26:37 +0530

    solved https://www.cses.fi/problemset/task/2216/

---
+++ CollectingNumbers.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class CollectingNumbers {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(num - 1)) {
+                set.remove(num - 1);
+            }
+            set.add(num);
+        }
+
+        out.println(set.size());
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
