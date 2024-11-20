b8de8b9838c190d2bf698a7f5ff09717dcdd7ef1
yash7xm <yash.7xm@gmail.com>
2024-11-20 17:47:08 +0530

    solved https://www.cses.fi/problemset/task/1074/

---
+++ Sorting_and_Searching/StickLengths.java
@@ -0,0 +1,57 @@
+package Sorting_and_Searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class StickLengths {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int median = arr[n / 2];
+
+        long cost = 0;
+        for (int i = 0; i < n; i++) {
+            cost += Math.abs(arr[i] - median);
+        }
+
+        out.println(cost);
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
