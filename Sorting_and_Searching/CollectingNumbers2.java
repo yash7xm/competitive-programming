a421ec4c447445f46c0dd884fdcb16326a6690ce
yash7xm <yash.7xm@gmail.com>
2024-11-22 12:10:58 +0530

    solved collecting numbers 2 cses

--- Sorting_and_Searching/CollectingNumbers2.java
+++ Sorting_and_Searching/CollectingNumbers2.java
@@ -1,7 +1,11 @@
 package Sorting_and_Searching;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class CollectingNumbers2 {
     static FastReader in = new FastReader();
