c14db5cdb6769b701ef676535efe96fd1a3a78fa
yash7xm <yash.7xm@gmail.com>
2024-12-19 15:44:05 +0530

    solved https://www.cses.fi/problemset/task/1631/

---
+++ ReadingBooks.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class ReadingBooks {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        long largest = arr[n - 1];
+        long sumOfRest = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sumOfRest += arr[i];
+        }
+
+        if (largest <= sumOfRest) {
+            out.println(largest + sumOfRest);
+        } else {
+            out.println(2 * largest);
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
