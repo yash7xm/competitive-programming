23abbff5420cd9e91ea54f385c7874f338c32ba0
yash7xm <yash.7xm@gmail.com>
2024-12-19 18:58:41 +0530

    solved https://www.cses.fi/problemset/task/1661

---
+++ SubarraySumsII.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class SubarraySumsII {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int tar = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        long curr = 0, cnt = 0;
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1);
+        for (int i = 0; i < n; i++) {
+            curr += arr[i];
+            cnt += map.getOrDefault(curr - tar, 0);
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
+        }
+
+        out.println(cnt);
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
