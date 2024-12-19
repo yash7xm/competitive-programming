78d890aca981d3203d4bc8335475cc8a41961f23
yash7xm <yash.7xm@gmail.com>
2024-12-19 18:34:25 +0530

    solved https://www.cses.fi/problemset/task/1660/

---
+++ SubarraySumsI.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class SubarraySumsI {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int tar = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        long curr = 0, cnt = 0;
+        for (int j = 0, i = 0; j < n; j++) {
+            curr += arr[j];
+            while (i < j && curr > tar) {
+                curr -= arr[i];
+                i++;
+            }
+            if (curr == tar)
+                cnt++;
+        }
+
+        out.println(cnt);
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
