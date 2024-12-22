29f54418742eb0bd97925ff4b994bd04578a1369
yash7xm <yash.7xm@gmail.com>
2024-12-22 20:23:45 +0530

    contest 2051 A

---
+++ PreparingfortheOlympiad.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PreparingfortheOlympiad {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            int[] brr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                brr[i] = in.nextInt();
+            }
+
+            int res = 0;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > brr[i + 1]) {
+                    res += arr[i] - brr[i+1];
+                }
+            }
+            res += arr[n - 1];
+            out.println(res);
+            out.flush();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ SubarrayDistinctValues.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class SubarrayDistinctValues {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            while (i < j && map.size() > k) {
+                map.put(arr[i], map.get(arr[i]) - 1);
+                if (map.get(arr[i]) == 0) {
+                    map.remove(arr[i]);
+                }
+                i++;
+            }
+            int len = (j - i);
+            cnt += (len * (len + 1)) / 2;
+            map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+        }
+
+        out.println(cnt);
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
