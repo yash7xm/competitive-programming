1b5e49ec9264f264d2b179c5688a7fb3766a503d
yash7xm <yash.7xm@gmail.com>
2024-12-23 15:14:15 +0530

    cf updated

--- tle-800/PreparingfortheOlympiad.java
+++ tle-800/PreparingfortheOlympiad.java
@@ -25,7 +25,7 @@ public class PreparingfortheOlympiad {
             int res = 0;
             for (int i = 0; i < n - 1; i++) {
                 if (arr[i] > brr[i + 1]) {
-                    res += arr[i] - brr[i+1];
+                    res += arr[i] - brr[i + 1];
                 }
             }
             res += arr[n - 1];
