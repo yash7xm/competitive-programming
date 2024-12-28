c6a9de7c98c182c2f952040a60e007aecf2b218f
yash7xm <yash.7xm@gmail.com>
2024-12-28 15:26:32 +0530

    solved https://cses.fi/problemset/task/1713/

---
+++ Math/CountingDivisors.java
@@ -0,0 +1,59 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CountingDivisors {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            out.println(solve(x));
+        }
+        out.flush();
+    }
+
+    public static int solve(int n) {
+        int cnt = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+                if (n / i != i) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
