f455db768e4a75713f32378012dcbccd60956fec
yash7xm <yash.7xm@gmail.com>
2024-12-29 15:54:42 +0530

    solved https://codeforces.com/problemset/problem/1389/A

---
+++ LCMProblem.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LCMProblem {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            long l = in.nextInt();
+            long r = in.nextInt();
+
+            if (2 * l > r) {
+                out.println(-1 + " " + -1);
+            } else {
+                out.println(l + " " + 2 * l);
+            }
+
+            out.flush();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
