41ae7af57819db1406d725d8b269c98b1f41e370
yash7xm <yash.7xm@gmail.com>
2024-12-29 17:30:22 +0530

    solved https://codeforces.com/problemset/problem/1732/A

---
+++ Bestie.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Bestie {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+            int g = arr[0];
+            for (int i = 1; i < n; i++) {
+                g = gcd(Math.max(g, arr[i]), Math.min(g, arr[i]));
+            }
+
+            long res = 0;
+            if (g != 1) {
+                if (gcd(g, n) == 1) {
+                    res = 1;
+                } else if (gcd(g, n - 1) == 1) {
+                    res = 2;
+                } else {
+                    res = 3;
+                }
+            }
+
+            out.println(res);
+            out.flush();
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
