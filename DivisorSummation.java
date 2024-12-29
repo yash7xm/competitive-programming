1a9753d7220c0e34b9e0ddcef90fbad5c083faf9
yash7xm <yash.7xm@gmail.com>
2024-12-29 18:47:46 +0530

    solved https://www.spoj.com/problems/DIVSUM/

---
+++ DivisorSummation.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DivisorSummation {
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            long sum = 0;
+            for (int i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    sum += i;
+                    if (n / i != i) {
+                        sum += n / i;
+                    }
+                }
+            }
+
+            out.println(sum - n);
+            out.flush();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
