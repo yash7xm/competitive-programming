ccfca7592c1ea5272b6a9f20221a1ebd8480c4ee
yash7xm <yash.7xm@gmail.com>
2025-01-04 18:22:45 +0530

    solved https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/D

---
+++ NumberOfSegmentsWithBigSum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberOfSegmentsWithBigSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long s = in.nextLong();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        long curr = 0, res = 0;
+        for (int i = 0, j = 0; j < n; j++) {
+            curr += arr[j];
+            while (curr >= s) {
+                res += n - j;
+                curr -= arr[i];
+                i++;
+            }
+        }
+
+        System.out.println(res);
+        in.close();
+    }
+}
