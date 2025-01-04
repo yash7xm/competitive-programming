94846f3d1c8136b6c1e42d358b83eb35ac947b4b
yash7xm <yash.7xm@gmail.com>
2025-01-04 17:45:02 +0530

    solved https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A

---
+++ SegmentWithSmallSum.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class SegmentWithSmallSum {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        long s = in.nextLong();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        long curr = 0;
+        int res = 0;
+        for (int j = 0, i = 0; j < n; j++) {
+            curr += arr[j];
+            while (curr > s) {
+                curr -= arr[i];
+                i++;
+            }
+
+            res = Math.max(res, j - i + 1);
+        }
+
+        out.println(res);
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
