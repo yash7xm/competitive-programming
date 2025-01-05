4c3d32509d2496f38f9d42177f0a7ca8795a86da
yash7xm <yash.7xm@gmail.com>
2025-01-05 19:16:47 +0530

    solved https://vjudge.net/problem/HackerRank-maximizing-xor

---
+++ MaximizingXOR.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MaximizingXOR {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        int r = in.nextInt();
+
+        int xor = l ^ r;
+        int msb = 0;
+        while (xor > 0) {
+            msb++;
+            xor >>= 1;
+        }
+
+        int res = (1 << msb) - 1;
+
+        System.out.println(res);
+        in.close();
+    }
+}
