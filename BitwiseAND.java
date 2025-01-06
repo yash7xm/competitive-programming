02995ca9a1ee2550c7ac42de9eb5ce31f68afd2e
yash7xm <yash.7xm@gmail.com>
2025-01-06 14:11:58 +0530

    solved https://vjudge.net/problem/HackerRank-30-bitwise-and

---
+++ BitwiseAND.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BitwiseAND {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+
+            int res = 0;
+
+            if (((k - 1) | k) <= n) {
+                res = k - 1;
+            } else {
+                res = k - 2;
+            }
+
+            System.out.println(res);
+        }
+        in.close();
+    }
+}
