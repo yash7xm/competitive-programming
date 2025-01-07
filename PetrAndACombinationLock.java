917d8d25379ccef365b29564bef965851831c468
yash7xm <yash.7xm@gmail.com>
2025-01-07 13:21:20 +0530

    solved https://codeforces.com/problemset/problem/1097/B

---
+++ PetrAndACombinationLock.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PetrAndACombinationLock {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < (1 << n); i++) {
+            long sum = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) == 0) {
+                    sum -= arr[j];
+                } else {
+                    sum += arr[j];
+                }
+            }
+
+            if (sum % 360 == 0) {
+                System.out.println("YES");
+                in.close();
+                return;
+            }
+        }
+
+        System.out.println("NO");
+        in.close();
+    }
+}
