5b8263ad2538e3e1736881ef5c3ecee14c90f0d7
yash7xm <yash.7xm@gmail.com>
2025-01-07 19:11:57 +0530

    solved https://www.spoj.com/problems/ABACABA/

---
+++ PrintThePattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintThePattern {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        System.out.println(print(n));
+        in.close();
+    }
+
+    static String print(int n) {
+        if (n == 1) {
+            return "A";
+        }
+
+        String previous = print(n - 1);
+        return previous + (char) (n + 'A' - 1) + previous;
+    }
+}
