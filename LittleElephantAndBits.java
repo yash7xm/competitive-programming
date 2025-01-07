feec6c557b8ea2d3c7e46a5acb3c5bbcac9af71b
yash7xm <yash.7xm@gmail.com>
2025-01-07 16:53:39 +0530

    solved https://codeforces.com/problemset/problem/258/A

---
+++ LittleElephantAndBits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LittleElephantAndBits {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int n = s.length();
+        int idx = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0') {
+                idx = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i != idx) {
+                System.out.print(s.charAt(i));
+            }
+        }
+        System.out.println();
+        in.close();
+    }
+}
