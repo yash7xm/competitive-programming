913ead0c1a5c046b1caa530e203b14a5c170cdf8
yash7xm <yash.7xm@gmail.com>
2025-01-07 17:36:33 +0530

    solved https://codeforces.com/problemset/problem/96/B

---
+++ LuckyNumbersEasy.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LuckyNumbersEasy {
+
+    private static ArrayList<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+
+        for (int i = 2; i <= 18; i += 2) {
+            generate("", i / 2, i / 2);
+        }
+
+        Collections.sort(list);
+
+        for (long num : list) {
+            if (num >= n) {
+                System.out.println(num);
+                break;
+            }
+        }
+
+        in.close();
+    }
+
+    static void generate(String curr, int count4, int count7) {
+        if (count4 == 0 && count7 == 0) {
+            list.add(Long.parseLong(curr));
+            return;
+        }
+
+        if (count4 > 0) {
+            generate(curr + "4", count4 - 1, count7);
+        }
+
+        if (count7 > 0) {
+            generate(curr + "7", count4, count7 - 1);
+        }
+    }
+}
