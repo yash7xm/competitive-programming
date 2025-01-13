55c04ec220e5f57952d4721338d9a29c490aff19
yash7xm <yash.7xm@gmail.com>
2025-01-13 11:24:19 +0530

    solved https://cses.fi/problemset/task/1070/

---
+++ Permutations.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Permutations {
+
+	static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        if(n == 1) {
+			out.println("1");
+		} else if(n <= 3) {
+			out.println("NO SOLUTION");
+		} else {
+			for(int i=2; i<=n; i+=2) {	
+				out.print(i+" ");
+			}
+			for(int i=1; i<=n; i+=2) {	
+				out.print(i+" ");
+			}
+		}
+		out.flush();
+    }
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
