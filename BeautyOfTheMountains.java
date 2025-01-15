745f985de5f017188fc2ad6c75667e8c955ab6d1
yash7xm <yash.7xm@gmail.com>
2025-01-15 16:11:10 +0530

    Format

--- BeautyOfTheMountains.java
+++ BeautyOfTheMountains.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class BeautyOfTheMountains {
-	
+
 	static FastReader in = new FastReader();
-    static PrintWriter out = new PrintWriter(System.out);
+	static PrintWriter out = new PrintWriter(System.out);
 
 	public static void main(String[] args) {
 		int t = in.nextInt();
@@ -20,88 +20,89 @@ public class BeautyOfTheMountains {
 		int k = in.nextInt();
 
 		int[][] a = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] = in.nextInt();
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				a[i][j] = in.nextInt();
+			}
+		}
 
-        String[] s = new String[n];
-        for (int i = 0; i < n; i++) {
-            s[i] = in.next();
-        }
+		String[] s = new String[n];
+		for (int i = 0; i < n; i++) {
+			s[i] = in.next();
+		}
 
-        int[][] pref = new int[n + 1][m + 1];
-        long diff = 0;
+		int[][] pref = new int[n + 1][m + 1];
+		long diff = 0;
 
-        for (int i = 0; i < n; i++) {
-            int cur = 0;
-            for (int j = 0; j < m; j++) {
-                if (s[i].charAt(j) == '1') {
-                    cur += 1;
-                    diff += a[i][j];
-                } else {
-                    diff -= a[i][j];
-                }
-                pref[i + 1][j + 1] = pref[i][j + 1] + cur;
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			int cur = 0;
+			for (int j = 0; j < m; j++) {
 
-        if (diff == 0) {
-            out.println("YES");
-            return;
-        }
+				if (s[i].charAt(j) == '1') {
+					cur += 1;
+					diff += a[i][j];
+				} else {
+					diff -= a[i][j];
+				}
+				pref[i + 1][j + 1] = pref[i][j + 1] + cur;
+			}
+		}
 
-        long g = 0;
+		if (diff == 0) {
+			out.println("YES");
+			return;
+		}
 
-        for (int i = 0; i <= n - k; i++) {
-            for (int j = 0; j <= m - k; j++) {
-                long f = pref[i + k][j + k] - pref[i + k][j] - pref[i][j + k] + pref[i][j];
-                f = Math.abs(k * k - 2 * f);
-                g = gcd(g, f);
-            }
-        }
+		long g = 0;
 
-        if (g == 0 || diff % g != 0) {
-            out.println("NO");
-        } else {
-            out.println("YES");
-        }
-    }
+		for (int i = 0; i <= n - k; i++) {
+			for (int j = 0; j <= m - k; j++) {
+				long f = pref[i + k][j + k] - pref[i + k][j] - pref[i][j + k] + pref[i][j];
+				f = Math.abs(k * k - 2 * f);
+				g = gcd(g, f);
+			}
+		}
 
-    public static long gcd(long a, long b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
+		if (g == 0 || diff % g != 0) {
+			out.println("NO");
+		} else {
+			out.println("YES");
+		}
+	}
+
+	public static long gcd(long a, long b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
 
 	// FastReader for faster input
 	static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+		BufferedReader br;
+		StringTokenizer st;
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
 
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
 }
--- ForestQueries1.java
+++ ForestQueries1.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class ForestQueries1 {
-	
+
 	static FastReader in = new FastReader();
-    static PrintWriter out = new PrintWriter(System.out);
+	static PrintWriter out = new PrintWriter(System.out);
 
 	public static void main(String[] args) {
 		solve();
@@ -14,11 +14,11 @@ public class ForestQueries1 {
 	public static void solve() {
 		int n = in.nextInt();
 		int q = in.nextInt();
-		int[][] arr = new int[n+1][n+1];
-		for(int i=1; i<=n; i++) {
+		int[][] arr = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
 			String row = in.next();
-			for(int j=1; j<=n; j++) {
-				if(row.charAt(j-1) == '.') {
+			for (int j = 1; j <= n; j++) {
+				if (row.charAt(j - 1) == '.') {
 					arr[i][j] = 0;
 				} else {
 					arr[i][j] = 1;
@@ -26,50 +26,50 @@ public class ForestQueries1 {
 			}
 		}
 
-		long[][] dp = new long[n+1][n+1];
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+		long[][] dp = new long[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
 			}
 		}
 
-		for(int i=0; i<q; i++) {
+		for (int i = 0; i < q; i++) {
 			int a1 = in.nextInt();
 			int b1 = in.nextInt();
 			int a2 = in.nextInt();
 			int b2 = in.nextInt();
 
 			long res = 0;
-			res = dp[a2][b2] - dp[a2][b1-1] - dp[a1-1][b2] + dp[a1-1][b1-1];
-			out.println(res);			
+			res = dp[a2][b2] - dp[a2][b1 - 1] - dp[a1 - 1][b2] + dp[a1 - 1][b1 - 1];
+			out.println(res);
 		}
 	}
 
 	static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+		BufferedReader br;
+		StringTokenizer st;
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
 
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
 }
