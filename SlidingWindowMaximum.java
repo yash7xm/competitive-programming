4c92f9f04dced7f4dc0ce99d471bee0cf151d2c8
yash7xm <yash.7xm@gmail.com>
2025-01-23 19:15:03 +0530

    solved https://www.hackerrank.com/contests/code-cpp-september-2015/challenges/deque-stl/problem

--- SlidingWindowMaximum.java
+++ SlidingWindowMaximum.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class SlidingWindowMaximum {
-	
+
 	static FastReader in = new FastReader();
-    static PrintWriter out = new PrintWriter(System.out);
+	static PrintWriter out = new PrintWriter(System.out);
 
 	public static void main(String[] args) {
 		int t = in.nextInt();
-		while(t-- > 0) {
+		while (t-- > 0) {
 			solve();
 		}
 		out.flush();
@@ -19,25 +19,25 @@ public class SlidingWindowMaximum {
 		int k = in.nextInt();
 
 		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = in.nextInt();
 		}
 
 		ArrayDeque<Integer> dq = new ArrayDeque<>();
-		for(int i=0; i<k; i++) {
-			while(!dq.isEmpty() && arr[i] > arr[dq.peekLast()]) {
+		for (int i = 0; i < k; i++) {
+			while (!dq.isEmpty() && arr[i] > arr[dq.peekLast()]) {
 				dq.pollLast();
 			}
 			dq.addLast(i);
 		}
 
-		for(int i=k; i<n; i++) {
+		for (int i = k; i < n; i++) {
 			out.print(arr[dq.peekFirst()] + " ");
-			if(!dq.isEmpty() && i - dq.peekFirst() >= k) {
+			if (!dq.isEmpty() && i - dq.peekFirst() >= k) {
 				dq.pollFirst();
 			}
 
-			while(!dq.isEmpty() && arr[i] > arr[dq.peekLast()]) {
+			while (!dq.isEmpty() && arr[i] > arr[dq.peekLast()]) {
 				dq.pollLast();
 			}
 			dq.addLast(i);
@@ -47,30 +47,30 @@ public class SlidingWindowMaximum {
 	}
 
 	static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+		BufferedReader br;
+		StringTokenizer st;
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
 
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
 }
