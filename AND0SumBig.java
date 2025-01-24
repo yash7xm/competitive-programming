d2d8436ee8b85d4395517b2ec604117b6b1348db
yash7xm <yash.7xm@gmail.com>
2025-01-24 16:49:25 +0530

    solved https://codeforces.com/problemset/problem/1514/B

---
+++ AND0SumBig.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class AND0SumBig {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		int n = in.nextInt();
+		int k = in.nextInt();
+
+		int mod = (int) 1e9 + 7;
+
+		long ans = 1;
+		for (int i = 0; i < k; i++) {
+			ans = (ans * n) % mod;
+		}
+
+		out.println(ans);
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
