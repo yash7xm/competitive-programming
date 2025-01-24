2a027300614e757e9db67213707b33f6f6770bb0
yash7xm <yash.7xm@gmail.com>
2025-01-25 00:27:23 +0530

    solved https://codeforces.com/problemset/problem/1682/B

---
+++ ANDSorting.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class ANDSorting {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		int n = in.nextInt();
+		int ans = (1 << 30) - 1;
+		for (int i = 0; i < n; ++i) {
+			int x = in.nextInt();
+			if (x != i) {
+				ans &= x;
+			}
+		}
+
+		out.println(ans);
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
