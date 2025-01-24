631648f1fafe063a7963bd7f3f3f9decd6261df5
yash7xm <yash.7xm@gmail.com>
2025-01-24 12:55:25 +0530

    solved https://codeforces.com/problemset/problem/484/A

---
+++ Bits.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Bits {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		long l = in.nextLong();
+		long r = in.nextLong();
+
+		out.println(findMaxBits(l, r));
+	}
+
+	private static long findMaxBits(long l, long r) {
+		if (l == r) {
+			return l;
+		}
+
+		long highestBitR = Long.highestOneBit(r);
+
+		if (highestBitR <= l) {
+			return findMaxBits(l - highestBitR, r - highestBitR) + highestBitR;
+		}
+
+		long fullSet = (highestBitR << 1) - 1;
+
+		if (fullSet <= r) {
+			return fullSet;
+		}
+
+		return highestBitR - 1;
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
