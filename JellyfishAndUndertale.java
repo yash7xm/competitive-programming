7bdf11a9e7a33ea97d8ddeda92bea4936b000c51
yash7xm <yash.7xm@gmail.com>
2025-01-26 13:31:52 +0530

    solved https://codeforces.com/problemset/problem/1875/A

---
+++ JellyfishAndUndertale.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class JellyfishAndUndertale {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int n = in.nextInt();
+
+		long ans = b;
+		for (int i = 0; i < n; i++) {
+			int x = in.nextInt();
+			ans += Math.min(a - 1, x);
+		}
+
+		out.println(ans);
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
