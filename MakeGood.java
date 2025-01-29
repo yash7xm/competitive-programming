438c674ded5ff593c447f2d17674d82b9a01f684
yash7xm <yash.7xm@gmail.com>
2025-01-30 00:21:18 +0530

    solved https://codeforces.com/problemset/problem/1270/C

---
+++ MakeGood.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class MakeGood {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		int n = in.nextInt();
+
+		long sum = 0;
+		long xor = 0;
+
+		for (int i = 0; i < n; i++) {
+			int x = in.nextInt();
+
+			sum += x;
+			xor ^= x;
+		}
+
+		out.println("2");
+		out.println(xor + " " + (sum + xor));
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
