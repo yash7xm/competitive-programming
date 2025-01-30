fec95fc1f23d1a7967455d95875d1554d1931bd2
yash7xm <yash.7xm@gmail.com>
2025-01-30 19:22:00 +0530

    solved Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class MissingNumber {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		solve();
+		out.flush();
+	}
+
+	public static void solve() {
+		int n = in.nextInt();
+		int xor = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			xor ^= i;
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			xor ^= in.nextInt();
+		}
+
+		out.println(xor);
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
