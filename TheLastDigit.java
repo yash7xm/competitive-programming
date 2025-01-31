e2ccfc8b74497239c3f370789b2000c8af88a2ea
yash7xm <yash.7xm@gmail.com>
2025-01-31 18:35:30 +0530

    solved https://www.spoj.com/problems/LASTDIG/

---
+++ TheLastDigit.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class TheLastDigit {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		while (t-- > 0) {
+			solve();
+		}
+		out.flush();
+	}
+
+	public static void solve() {
+		long a = in.nextLong();
+		long b = in.nextLong();
+
+		out.println(exp(a, b, 10L));
+	}
+
+	public static long exp(long a, long b, long mod) {
+		long res = 1;
+		a %= mod;
+		while (b > 0) {
+			if ((b & 1) == 1) {
+				res = res * a % mod;
+			}
+			a = a * a % mod;
+			b >>= 1;
+		}
+		return res;
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
