d6927bf6db4aa0a8181961c9875d3f1475c50ad9
yash7xm <yash.7xm@gmail.com>
2025-02-01 13:05:16 +0530

    solved https://vjudge.net/problem/Gym-248968S

---
+++ FactorialUnderModulo.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class FactorialUnderModulo {
+
+	static FastReader in = new FastReader();
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		solve();
+		out.flush();
+	}
+
+	public static void solve() {
+		int n = in.nextInt();
+		long[] fact = new long[n + 1];
+		int mod = 998244353;
+		fact[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			fact[i] = fact[i - 1] * i % mod;
+		}
+
+		out.println(fact[n]);
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
